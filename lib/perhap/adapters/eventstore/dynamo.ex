c7fa085414a6f4b53f48f9e02da5d9e5b1438090
Alan Dyer <alan@stonedust.ca>
2017-10-16 15:33:36 -0700

    adds batch get

--- lib/perhap/adapters/eventstore/dynamo.ex
+++ lib/perhap/adapters/eventstore/dynamo.ex
@@ -10,7 +10,10 @@ defmodule Perhap.Adapters.Eventstore.Dynamo do
   end
 
   def put_event(event) do
-    ExAws.Dynamo.put_item("Events", event)
+    ExAws.Dynamo.put_item("Events", %{Map.from_struct(event) | metadata: Map.from_struct(event.metadata)})
+    |> ExAws.request!
+
+    ExAws.Dynamo.put_item("Index", %{context: event.metadata.context, entity_id: event.metadata.entity_id, event_id: event.event_id})
     |> ExAws.request!
 
     :ok
@@ -22,7 +25,7 @@ defmodule Perhap.Adapters.Eventstore.Dynamo do
 
     case dynamo_object do
       %{"Item" => data} ->
-        metadata = ExAws.Dynamo.decode_item(Map.get(dynamo_object, "Item") |> Map.get("metadata"), as: Perhap.Event.Metadata)
+        metadata = ExAws.Dynamo.decode_item(Map.get(data, "metadata"), as: Perhap.Event.Metadata)
 
         event = ExAws.Dynamo.decode_item(dynamo_object, as: Perhap.Event)
 
@@ -33,6 +36,38 @@ defmodule Perhap.Adapters.Eventstore.Dynamo do
   end
 
   def get_events(context, opts \\ []) do
-    {:ok, :thng}
+    event_ids = case Keyword.has_key?(opts, :entity_id) do
+      true ->
+        dynamo_object = ExAws.Dynamo.get_item("Index", %{context: context, entity_id: opts[:entity_id]})
+        |> ExAws.request!
+
+        case dynamo_object do
+          %{"Item" => data} ->
+            Map.get(data, "event_id", [])
+          %{} ->
+            {:error, "Event not Found"}
+        end
+      _ ->
+        dynamo_object = ExAws.Dynamo.query("Index", [key_condition_expression: "context = #{context}"])
+        |> ExAws.request! |> ExAws.Dynamo.decode_item
+    end
+
+    event_ids2 = case Keyword.has_key?(opts, :after) do
+      true ->
+        after_event = time_order(opts[:after])
+        event_ids |> Enum.filter(fn {ev} -> ev > after_event end)
+      _ -> event_ids
+    end
+
+    #possible this can only do 100 at a time, run through a loop if more
+    events = ExAws.Dynamo.batch_get_item(%{"Events" => [keys: event_ids2]})
+    {:ok, events}
+  end
+
+  defp time_order(maybe_uuidv1) do
+    case Perhap.Event.is_time_order?(maybe_uuidv1) do
+      true -> maybe_uuidv1
+      _ -> maybe_uuidv1 |> Perhap.Event.uuid_v1_to_time_order
+    end
   end
 end
